d7cbbe801328a662fefed66c66b8d268d5114c1f
kotkotei <kotkotei@yandex.ru>
2021-05-21 16:16:28 +0700

    gteioe

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ВАРИАНТ 10 Задание 1 Дано натуральное число.Определить произведение его цифр,больших семи;
+            Console.WriteLine("Введите наруто число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int b = 0;
+            int s = 1;
+            while (n > 0)
+            {
+                b = n % 10;
+                n = n / 10;
+                if (b > 7)
+                {
+                    s = s * b;
+                }
+                Console.WriteLine(s);
+                Console.ReadKey();
+
+
+
+            }
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите наруто число");
+            int a = Convert.ToInt32(Console.ReadLine()); ;
+            int i = 2;
+            while (i < a)
+            {
+                i++;
+             }
+            Console.WriteLine(i);
+            Console.ReadKey();
+            
+        }
+    }
+}
